b12a8eab2955277d07f564c824aad9ace3fdccf6
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-27 21:15:39 -0500

    se crea la lista se subtareas tasklist en la funcion reducer

--- front/src/Components/Reducer.js
+++ front/src/Components/Reducer.js
@@ -1,38 +1,58 @@
 function Reducer(state, action) {
     switch (action.type) {
-      case 'update-item':
-        const todoUpItem = state.todo;
-        const listUpdateEdit = todoUpItem.list.map((item) => {
-          if (item.id === action.item.id) {
-            return action.item;
-          }
-          return item;
-        });
-        todoUpItem.list = listUpdateEdit;
-        todoUpItem.item = {};
-        return { ...state, todo: todoUpItem }
-      case 'delete-item':
-        const todoUpDelete = state.todo;
-        const listUpdate = todoUpDelete.list.filter((item) => {
-          return item.id !== action.id;
-        });
-        todoUpDelete.list = listUpdate;
-        return { ...state, todo: todoUpDelete }
-      case 'update-list':
-        const todoUpList = state.todo;
-        todoUpList.list = action.list;
-        return { ...state, todo: todoUpList }
-      case 'edit-item':
-        const todoUpEdit = state.todo;
-        todoUpEdit.item = action.item;
-        return { ...state, todo: todoUpEdit }
-      case 'add-item':
-        const todoUp = state.todo.list;
-        todoUp.push(action.item);
-        return { ...state, todo: {list: todoUp, item: {}} }
-      default:
-        return state;
+
+        case 'update-tasklist':
+			const taskUpdateList = state.task;
+			taskUpdateList.taskList = action.taskList;
+			return { ...state, task: taskUpdateList }
+		case 'add-task':
+			const newTaskList = state.task.taskList;
+			newTaskList.push(action.item);
+			return { ...state, task: { taskList: newTaskList, item: {} } }
+
+		case 'delete-task':
+			const taskDeleteItem = state.task;
+			const taskListUpdate = taskDeleteItem.taskList.filter((item) => {
+				return item.id !== action.id;
+			});
+			taskDeleteItem.taskList = taskListUpdate;
+			return { ...state, task: taskDeleteItem }
+
+
+
+        case 'update-item':
+            const todoUpItem = state.todo;
+            const listUpdateEdit = todoUpItem.list.map((item) => {
+            if (item.id === action.item.id) {
+                return action.item;
+            }
+            return item;
+            });
+            todoUpItem.list = listUpdateEdit;
+            todoUpItem.item = {};
+            return { ...state, todo: todoUpItem }
+        case 'delete-item':
+            const todoUpDelete = state.todo;
+            const listUpdate = todoUpDelete.list.filter((item) => {
+            return item.id !== action.id;
+            });
+            todoUpDelete.list = listUpdate;
+            return { ...state, todo: todoUpDelete }
+        case 'update-list':
+            const todoUpList = state.todo;
+            todoUpList.list = action.list;
+            return { ...state, todo: todoUpList }
+        case 'edit-item':
+            const todoUpEdit = state.todo;
+            todoUpEdit.item = action.item;
+            return { ...state, todo: todoUpEdit }
+        case 'add-item':
+            const todoUp = state.todo.list;
+            todoUp.push(action.item);
+            return { ...state, todo: {list: todoUp, item: {}} }
+        default:
+            return state;
     }
-  }
+}
 
   export default Reducer;
